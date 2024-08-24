19b6ba0b8857b66834afba7d1391fa0d6cc5002d
Dmitry Savkov <dmitry_savkov@mail.ru>
2024-08-24 21:16:06 +0500

    added flush to prints

--- messenger/routes.py
+++ messenger/routes.py
@@ -260,12 +260,12 @@ def callback():
     access_token = create_jwt_token(user.user_id, ACCESS_TOKEN_EXPIRATION)
     refresh_token = create_jwt_token(user.user_id, REFRESH_TOKEN_EXPIRATION, refresh=True)
 
-    print(access_token, refresh_token)
+    print(access_token, refresh_token, flush=True)
 
     # TODO is there any way not to replicate this?
-    print(url_for('home'))
+    print(url_for('home'), flush=True)
     response = redirect('https://messenger-s3fg.onrender.com/home')
-    print(response, url_for('home'))
+    print(response, url_for('home'), flush=True)
     response.set_cookie('access-token', access_token, httponly=True)
     response.set_cookie('refresh-token', refresh_token, httponly=True)
 
