b4fb74bad7aa115ff46438dbc0057208fc2f64e8
Dmitry Savkov <dmitry_savkov@mail.ru>
2025-08-25 19:21:18 +0500

    fix: added localhost for mongo as fallback

--- messenger/__init__.py
+++ messenger/__init__.py
@@ -9,6 +9,7 @@ from redis import Redis
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
+LOCAL_MONGO_URI = 'mongodb://localhost:27017/messenger_db'
 
 # For using .env variables
 load_dotenv()
@@ -50,7 +51,11 @@ print(redis, flush=True)
 # Every deletion, editing and adding touches every user.
 # I set message and author: user_id in group_chats. On get history and check and add a css-class=mine when equaled.
 # To implement delete, edit with only by yourself a set with (user_id, message (by author)) should be created and checked every time.
-mongo_cluster = MongoClient(os.getenv('MONGO_PRODUCTION_CLUSTER_URI'))
+mongo_uri = os.getenv(
+    'MONGO_PRODUCTION_CLUSTER_URI',
+    LOCAL_MONGO_URI
+)
+mongo_cluster = MongoClient(mongo_uri)
 mongo_db = mongo_cluster['messenger']
 group_chats = mongo_db['groups_history']
 contact_chats = mongo_db['contacts_history']
